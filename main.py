b6df452a43679ae199c1827598c753b7a795cafa
AydarKhisamov <157051549+AydarKhisamov@users.noreply.github.com>
2026-05-20 23:45:49 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ import pandas as pd
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import root_mean_squared_error
 from catboost import CatBoostRegressor
 
 # фиксация конфигов
